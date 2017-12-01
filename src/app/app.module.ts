ea0afdec08dfd509820f67bd551ddd37f6b40a9d
Dany Cortes <daniela.munozc@gmail.com>
2017-11-30 20:08:01 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChecklistComponent } from './core/common/checklist/checklist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChecklistComponent
   ],
   imports: [
     BrowserModule
